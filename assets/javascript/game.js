a0297b01ffa6b2877648aec600cdb5e7f743d435
Brendan Rogers <brendan@utexas.edu>
2017-03-19 23:35:36 -0500

    Set up first jQuery functions to hide available characters once main character is chosen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,5 +3,25 @@ $(document).ready(function(){
    $("#obiWan").click(function(){
         $("#lukeSkywalker, #darthSidious, #darthMaul").hide();
         console.log("hello");
-    });   
+   });
+    
+   
+
+    $("#lukeSkywalker").click(function(){
+        $("#obiWan, #darthSidious, #darthMaul").hide();
+   
+      });
+    
+
+    $("#darthSidious").click(function(){
+        $("#obiWan, #lukeSkywalker, #darthMaul").hide();
+    
+});
+    
+
+    $("#darthMaul").click(function(){
+        $("#obiWan, #darthSidious, #lukeSkywalker").hide();
+ 
+
+  });
 });
