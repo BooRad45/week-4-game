288248f1f42c70f8693390f100822f0805006f1b
Brendan Rogers <brendan@utexas.edu>
2017-03-20 19:43:10 -0500

    Finished basic jQuery commands for Your Character section. Need to align characters to left side of screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $("#lukeSkywalker, #darthSidious, #darthMaul").hide();//hide unpicked main characters
         console.log("hello"); //just checking things 
         $("#obiWanEnemy").css({"visibility": "hidden"});//hide main character in enemy section
-        $( "#enemiesAvailable" ).css({ "visibility": "visible"});//make unpicked characters visible in enemy section
+        $("#enemiesAvailable").css({ "visibility": "visible"});//make unpicked characters visible in enemy section
         $("#lukeSkywalkerEnemy, #darthSidiousEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
    }); // close of Obi-Wan main character functions
 
@@ -18,23 +18,33 @@ $(document).ready(function(){
    $("#lukeSkywalker").click(function(){ // Luke Skywalker event listener
         $("#obiWan, #darthSidious, #darthMaul").hide(); //hide unpicked main characters 
         $("#lukeSkywalkerEnemy").css({"visibility": "hidden"});//hide main character in enemy section
-        $( "#enemiesAvailable" ).css({ "visibility": "visible"});//make unpicked characters visible in enemy section
+        $("#enemiesAvailable").css({ "visibility": "visible"});//make unpicked characters visible in enemy section
         $("#obiWanEnemy, #darthSidiousEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
    }); // close of Luke Skywalker main character functions
      
    // CHARACTER #3 = DARTH SIDIOUS --- main character functions
    //!! Need function to slide enemy characters together and float left
     $("#darthSidious").click(function(){
-        $("#obiWan, #lukeSkywalker, #darthMaul").hide();
-        $("#darthSidious").animate({"right":"170px"}, "slow");
-    
-});
-    
-
+        $("#obiWan, #lukeSkywalker, #darthMaul").hide(); //hide unpicked main characters
+        $("#darthSidiousEnemy").css({"visibility": "hidden"});//hide main character in enemy section
+        $("#enemiesAvailable").css({ "visibility": "visible"});//make unpicked characters visible in enemy section
+        $("#obiWanEnemy, #lukeSkywalkerEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
+   }); // close of Darth Sidious main character functions
+          // $("#darthSidious").animate({"right":"170px"}, "slow");
+      
     $("#darthMaul").click(function(){
         $("#obiWan, #darthSidious, #lukeSkywalker").hide();
-        $("#darthMaul").animate({"right":"320px"}, "slow");
+        $("#darthMaulEnemy").css({"visibility": "hidden"});//hide main character in enemy section
+        $("#enemiesAvailable").css({ "visibility": "visible"});//make unpicked characters visible in enemy section
+        $("#obiWanEnemy, #lukeSkywalkerEnemy, #darthSidiousEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
+   }); // close of Darth Maul main character functions
+
+
+
+
+
+        // $("#darthMaul").animate({"right":"320px"}, "slow");
  
 
-  });
-});
+  }); //document.ready function close
+
