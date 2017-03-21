af82d9dbaee4cc752706c517765585154eb4f061
Brendan Rogers <brendan@utexas.edu>
2017-03-20 20:44:40 -0500

    Changed margin on LukeEnemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,16 @@ $(document).ready(function(){
 //YOUR CHARACTER functions (Need functions for situations where main character is chosen)
 
     // CHARACTER #1 = OBI-WAN ---  main character functions
-    ///////////////////////////////////////////////////////////////////////
    $("#obiWan").click(function(){  //Obi-Wan event listener
         $("#lukeSkywalker, #darthSidious, #darthMaul").hide();//hide unpicked main characters
         console.log("hello"); //just checking things 
         $("#obiWanEnemy").css({"display": "none"});//hide main character in enemy section
-        $("#enemiesAvailable").css({ "visibility": "visible", "margin-left": "-12%"});//make unpicked characters visible in enemy section
+        $("#enemiesAvailable").css({ "visibility": "visible", "margin-left": "-10%"});//make unpicked characters visible in enemy section
         $("#lukeSkywalkerEnemy, #darthSidiousEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
 
    }); // close of Obi-Wan main character functions
 
    // CHARACTER #2 = LUKE SKYWALKER --- main character functions
-   //!! Need function to slide enemy characters together and float left
    $("#lukeSkywalker").click(function(){ // Luke Skywalker event listener
         $("#obiWan, #darthSidious, #darthMaul").hide(); //hide unpicked main characters 
         $("#lukeSkywalkerEnemy").css({"display": "none"});//hide main character in enemy section
@@ -26,7 +24,6 @@ $(document).ready(function(){
    }); // close of Luke Skywalker main character functions
      
    // CHARACTER #3 = DARTH SIDIOUS --- main character functions
-   //!! Need function to slide enemy characters together and float left
     $("#darthSidious").click(function(){
         $("#obiWan, #lukeSkywalker, #darthMaul").hide(); //hide unpicked main characters
         $("#darthSidiousEnemy").css({"visibility": "hidden"});//hide main character in enemy section
@@ -36,8 +33,9 @@ $(document).ready(function(){
         $("#lukeSkywalkerEnemy").animate({"right":"6px"}, "slow");
         $("#darthSidious").animate({"right":"166px"}, "slow");
    }); // close of Darth Sidious main character functions
-          
-      
+  
+
+   // CHARACTER #4 = DARTH MAUL --- main character functions             
     $("#darthMaul").click(function(){
         $("#obiWan, #darthSidious, #lukeSkywalker").hide();
         $("#darthMaulEnemy").css({"visibility": "hidden"});//hide main character in enemy section
@@ -47,6 +45,10 @@ $(document).ready(function(){
         $("#darthSidiousEnemy").animate({"right":"7px"}, "slow");
         $("#darthMaul").animate({"right":"321px"}, "slow");
    }); // close of Darth Maul main character functions
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // ENEMIES AVAILABLE TO ATTACK functions (set up all scenarios when defender is chosen)
+    $("")
 
 
 
