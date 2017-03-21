b918d1868afd84e44e0e50d60ddbaa1ad869c505
Brendan Rogers <brendan@utexas.edu>
2017-03-20 19:57:18 -0500

    Set enemies to line up correctly on pick of ObiWan

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,9 +8,10 @@ $(document).ready(function(){
    $("#obiWan").click(function(){  //Obi-Wan event listener
         $("#lukeSkywalker, #darthSidious, #darthMaul").hide();//hide unpicked main characters
         console.log("hello"); //just checking things 
-        $("#obiWanEnemy").css({"visibility": "hidden"});//hide main character in enemy section
-        $("#enemiesAvailable").css({ "visibility": "visible"});//make unpicked characters visible in enemy section
+        $("#obiWanEnemy").css({"display": "none"});//hide main character in enemy section
+        $("#enemiesAvailable").css({ "visibility": "visible", "margin-left": "-12%"});//make unpicked characters visible in enemy section
         $("#lukeSkywalkerEnemy, #darthSidiousEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
+
    }); // close of Obi-Wan main character functions
 
    // CHARACTER #2 = LUKE SKYWALKER --- main character functions
