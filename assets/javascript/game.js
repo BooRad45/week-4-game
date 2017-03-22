0c8985a0c08fffc13cabe82778c3f11a436318d7
Brendan Rogers <brendan@utexas.edu>
2017-03-21 23:54:42 -0500

    set up conditions for four battles. Still more to add but submitting before deadline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
 
+
 ///////////GLOBAL VARIABLES FOR POWERS ////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
+    var HealthPoints = {                //creating objects to draw from during game
+                    ObiWan: 120,
+                    LukeSkywalker: 100,
+                    DarthSidious: 150,
+                    DarthMaul: 180
+                };
+    var AttackPointsBase = {
+                    ObiWan: 8,
+                    LukeSkywalker: 5,
+                    DarthSidious: 10,
+                    DarthMaul: 15
+                };
+    var CounterAttackPower = {
+                    ObiWan: 10,
+                    LukeSkywalker: 5,
+                    DarthSidious: 20,
+                    DarthMaul: 25
+                };
 
     var obiwangood; 
     var obiwanenemy;
@@ -11,8 +30,8 @@ $(document).ready(function(){
     var darthsidiousenemy;
     var darthmaulgood;
     var darthmaulenemy;
-
-
+ 
+  
 //YOUR CHARACTER functions (Need functions for situations where main character is chosen)
 
     // CHARACTER #1 = OBI-WAN ---  main character functions
@@ -23,6 +42,7 @@ $(document).ready(function(){
         $("#enemiesAvailable").css({ "visibility": "visible", "margin-left": "-10%"});//make unpicked characters visible in enemy section
         $("#lukeSkywalkerEnemy, #darthSidiousEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
         $("#obiWan").data('clicked', true);
+        
    }); // close of Obi-Wan main character functions
 
    // CHARACTER #2 = LUKE SKYWALKER --- main character functions
@@ -33,9 +53,8 @@ $(document).ready(function(){
         $("#obiWanEnemy, #darthSidiousEnemy, #darthMaulEnemy").css({"border":"red 10px solid"}); //place red border around enemy characters     
         $("#darthSidiousEnemy").animate({"right":"170px"}, "slow");
         $("#darthMaulEnemy").animate({"right":"175px"}, "slow");
-        $('#lukeSkywalker').on('click change', function() {
         $("#lukeSkywalker").data('clicked', true);
-});
+
 
    }); // close of Luke Skywalker main character functions
      
@@ -48,6 +67,7 @@ $(document).ready(function(){
         $("#darthMaulEnemy").animate({"right":"175px"}, "slow");
         $("#lukeSkywalkerEnemy").animate({"right":"6px"}, "slow");
         $("#darthSidious").animate({"right":"166px"}, "slow");
+        $("#darthSidious").data('clicked', true);
         // $("#lukeSkywalker").data('clicked', true);
    }); // close of Darth Sidious main character functions
   
@@ -61,6 +81,7 @@ $(document).ready(function(){
         $("#lukeSkywalkerEnemy").animate({"right":"4px"}, "slow");
         $("#darthSidiousEnemy").animate({"right":"7px"}, "slow");
         $("#darthMaul").animate({"right":"321px"}, "slow");
+        $("#darthMaul").data('clicked', true);
         // $("#lukeSkywalker").data('clicked', true);
 
    }); // close of Darth Maul main character functions
@@ -75,6 +96,7 @@ $(document).ready(function(){
         $("#lukeSkywalkerDef").animate({"right":"95%"}, "slow"); // align Luke left
         $("#darthSidiousEnemy").animate({"right":"91%"}, "slow");
         $("#darthMaulEnemy").animate({"right":"91%"}, "slow");
+        $("#lukeSkywalkerEnemy").data('clicked', true);
     }); //close of Luke Skywalker enemy functions
 
     //ObiWan YourCharacter and Darth Sidious Defender
@@ -84,6 +106,7 @@ $(document).ready(function(){
         $("#darthSidiousDef").animate({"right":"195%"}, "slow"); // align Darth Sidious left
         // $("#lukeSkywalkerEnemy").animate({"right":"10%"}, "slow");
         $("#darthMaulEnemy").animate({"right":"95%"}, "slow");
+        $("#darthSidiousEnemy").data('clicked', true);
     }); //close of Darth Sidious enemy functions
 
     //ObiWan YourCharacter and Darth Maul Defender
@@ -91,8 +114,7 @@ $(document).ready(function(){
         $("#darthMaulEnemy").css({"display": "none"});//hide main character in enemy section
         $("#darthMaulDef").css({ "visibility": "visible", "border": "black 10px solid"});//make Darth Maul visible in defender section
         $("#darthMaulDef").animate({"right":"292%"}, "slow"); // align Darth Sidious left
-        // $("#lukeSkywalkerEnemy").animate({"right":"10%"}, "slow");
-        // $("#lukeSkywalkerEnemy").animate({"right":"95%"}, "slow");
+        $("#darthMaulEnemy").data('clicked', true);
     }); //close of Darth Maul enemy functions
 
     //Luke Skywalker YourCharacter and Obi-Wan Defender
@@ -101,6 +123,7 @@ $(document).ready(function(){
         $("#obiWanDef").css({ "visibility": "visible", "border": "black 10px solid"});//make ObiWan visible in defender section
         $("#darthSidiousEnemy").animate({"right":"200%"}, "slow"); 
         $("#darthMaulEnemy").animate({"right":"200%"}, "slow");
+        $("#obiWanEnemy").data('clicked', true);
         // $("#lukeSkywalkerEnemy").animate({"right":"95%"}, "slow");
     }); //close of ObiWan enemy functions
 
@@ -140,18 +163,44 @@ $(document).ready(function(){
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 //////ATTACK BUTTON ACTIONS /////////////////////////////////////////////////////////////////
-///// OBI-WAN YOUR CHARACTER ///////////////////////////////
+///// OBI-WAN - YOUR CHARACTER ///////////////////////////////
     $("#attack").click(function(){
-        if ($("#obiWan").data("clicked") === true) {
-            console.log("It's Obi");
+        if ($("#obiWan").data("clicked") === true && ($("#lukeSkywalkerEnemy").data("clicked") === true)) {
+            console.log("First Battle scenario achieved"); // setting up battle between Good Obi and bad Luke
+
         }
-        else if ($("#lukeSkywalker").data("clicked") === true) {
-            console.log("It's Luke");
-
-    } 
-
+        
+        
 
   });
+///// LUKE SKYWALKER - YOUR CHARACTER ///////////////////////////////////////////////
+$("#attack").click(function(){
+        if ($("#lukeSkywalker").data("clicked") === true && ($("#obiWanEnemy").data("clicked") === true)) {
+            console.log("Second Battle scenario achieved"); //testing if condition is set for good Luke Bad Obi
+
+        }
+
+  });
+///// DARTH SIDIOUS ///////////////////////////////////////////////////////////////////
+
+$("#attack").click(function(){
+        if ($("#darthSidious").data("clicked") === true && ($("#darthMaulEnemy").data("clicked") === true)) {
+            console.log("Third Battle scenario achieved");//testing to set up condition for Good Sid bad Maul
+
+    }
+
+  });
+///// DARTH MAUL ///////////////////////////////////////////////////////////////////
+$("#attack").click(function(){
+        if ($("#darthMaul").data("clicked") === true && ($("#obiWanEnemy").data("clicked") === true)) {
+            console.log("Fourth Battle scenario achieved");
+
+  }
+
+ });
+
+});  //document.ready function close
 
 
-});//document.ready function close
+
+
